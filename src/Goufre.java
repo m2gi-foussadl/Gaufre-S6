be5473cd3aa2f991cdf52a5870de2756f8529c78
Ascanaki <lancelin.foussadier@gmail.com>
2023-04-21 11:43:08 +0200

    Début IHM fenetre principale

--- src/Goufre.java
+++ src/Goufre.java
@@ -1,5 +1,15 @@
+package src;
+
+import src.IHM.FenetreGraphique;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class Goufre {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FontFormatException {
         System.out.println("Miam miam miam");
+        FenetreGraphique fenetre1 = new FenetreGraphique();
+        fenetre1.setVisible(true);
+
     }
 }

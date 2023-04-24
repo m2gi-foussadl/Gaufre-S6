9df7cfacef943f8584f7485e163094e77062428b
hounyj <Julien.houny@gmail.com>
2023-04-24 12:44:42 +0200

    RE :Plateau avec un turnPlayer (1 ou 2) + le getter

--- src/Modele/Plateau.java
+++ src/Modele/Plateau.java
@@ -76,6 +76,10 @@ public class Plateau {
         return plateau;
     }
 
+    public int getTurnPlayer(){
+        return turnPlayer;
+    }
+
     public void afficher() {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
